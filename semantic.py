65bce9ef729c258e9f55250a299e0ba510643ada
_shubham_ <112776695+ShubhamNegi4@users.noreply.github.com>
2025-05-25 17:25:39 +0530

    added type chceking to the semantic analysis.py

--- semantic.py
+++ semantic.py
@@ -6,38 +6,144 @@ class SemanticError(Exception):
 
 
 #       checks for undeclared variables and scope of variables        
-def DelarationAndScope(node,symtab):
+def DelarationAndScope(node,SymbolTable):
         # Handle lists (e.g., statement_list)
         if isinstance(node, list):
+
                 for child in node:
-                        DelarationAndScope(child,symtab)
+                        DelarationAndScope(child,SymbolTable)
                 return
-        #       cheek for scope and declaration
+        
+        #       check for scope and declaration
         elif node.type == "input":
+
+                #       INPUT ID: Declare the variable
                 child = node.children[0]
-                if child.value not in symtab:
-                      symtab.add(child.value)
+                #       it declared just reinitialized else declared
+                if child.value not in SymbolTable:
+                      SymbolTable.add(child.value)
                 return
         
         elif node.type == "output":
+
+                #       PRINT term_low: Check the expression
                 child = node.children[0]
-                if child.type == "variable" and child.value not in symtab:
-                      raise SemanticError(f"{child.value} variable not declared")
+                #       variables used here should be declared
+                DelarationAndScope(child,SymbolTable)
+                return
                 
         elif node.type == "assignment":
-                child = node.children[0]
-                if child.value not in symtab:
-                      symtab.add(child.value)
+
+                #       SET ID TO term_low:
+                variable = node.children[0]
+                expression = node.children[1]
+                #        it declared just reinitialized else declared
+                if variable.value not in SymbolTable:
+                      SymbolTable.add(variable.value)
+                #        variables used here should be declared
+                DelarationAndScope(expression,SymbolTable)
+                return
         
         elif node.type == "variable":
-               if node.value not in symtab:
+               if node.value not in SymbolTable:
                       raise SemanticError(f"{node.value} variable not declared")
         
+        elif node.type == 'number' or node.type == "string":
+                #       ignore the literals they need not to be defined
+                return
+        
         # Recurse on children
-        i = (0 if node.type != "assignment" else 1)
-        while i < len(node.children):
-                DelarationAndScope(node.children[i],symtab)
-                i += 1
+        for child in node.children:
+                #       if it is any other type like operator , if , while
+                #       etc then just check th evariables
+                DelarationAndScope(child,SymbolTable)
+
+
+'''
+        NOTE : since INPUT x is of type unknown , we introduced
+        the === operator hence the type of input is kept "STRING"
+        initially but later if it is chceked for integer ( x === 1)
+        we change it to integer
+'''
+
+def TypeChecks(node,SymbolTable):
+        # Handle lists (e.g., statement_list)
+        if isinstance(node, list):
+
+                for child in node:
+                        TypeChecks(child,SymbolTable)
+                return None
+        
+        #       declaration part of variable
+        if node.type == "input":
+
+                #       INPUT ID: Declare the variable as string for now
+                child = node.children[0]
+                if child.value not in SymbolTable:
+                      SymbolTable[child.value] = "string"
+                return None
+        
+        elif node.type == "assignment":
+
+                #       SET ID TO term_low: the type has to be type of term_low
+                variable = node.children[0]
+                expression = node.children[1]
+                #        it declared just reinitialized else declared
+                if variable.value not in SymbolTable:
+                      SymbolTable[variable.value] = TypeChecks(expression,SymbolTable)
+                return None
+        
+        elif node.type == "output":
+
+                #       PRINT term_low: Check the expression
+                child = node.children[0]
+                #       variables used here should be declared
+                TypeChecks(child,SymbolTable)
+                return None
+        
+        elif node.type == "string":
+
+                return "string"
+        elif node.type == "number":
+
+                return "number"
+        elif node.type == "variable":
+
+                '''       since we already checked for decleration 
+                          and handled dynamic type our variable here has to have a type
+                '''
+                return SymbolTable[node.value]
+        
+        #       the actual type chcek at the opeara
+        # tors
+        elif node.type == "operator":
+
+                leftChild = node.children[0]
+                rightChild = node.children[1]
+
+                # the type change mentioned at NOTE
+                if node.value == "===":
+                        #       left child will always be a variable
+                        SymbolTable[leftChild.value] = rightChild.type
+                else:
+                        # get the types
+                        leftType = TypeChecks(leftChild,SymbolTable)
+                        rightType = TypeChecks(rightChild,SymbolTable)
+
+                        if leftType != rightType:
+                                raise SemanticError(f"Type mismatch : {leftType} {node.value} {rightType}")
+
+                        return leftType
+
+
+        
+         # Recurse on children
+        for child in node.children:
+                #       if it is any other type like if , while
+                #       etc then just check th evariables
+                TypeChecks(child,SymbolTable)
+        return None
+
 
         
 
@@ -45,8 +151,13 @@ def DelarationAndScope(node,symtab):
 def SemanticAnalysis(ast):
         try:
                 # 1. check for undeclared variables
+                SymbolTable = set()
+                DelarationAndScope(ast,SymbolTable)
+
                 # 2. check for scope of variables 
-                DelarationAndScope(ast,set())
+                SymbolTable = {}
+                TypeChecks(ast,SymbolTable)
+
                 return True
         except SemanticError as e:
                 print(f"Caught custom error: {e}")
